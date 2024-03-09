1c92eb17e019e6187e5b4fa9a63fec1f1a543c57
Markus Seeli <markus@seeli.eu>
2024-03-09 11:13:34 +0100

    Adjustments for exercises

--- dbSeeder.js
+++ dbSeeder.js
@@ -1,16 +1,14 @@
-'use strict';
-var dataInitializer = require('./lib/dataSeeder'),
-    config = require('./config/config.development.json'),
-    db = require('./lib/database');
+"use strict";
+var dataInitializer = require("./lib/dataSeeder"),
+  db = require("./lib/database");
 
-db.init(config.databaseConfig);
+db.init();
 
-console.log('Initializing Data');
-dataInitializer.initializeData(function(err) {
+console.log("Initializing Data");
+dataInitializer.initializeData(function (err) {
   if (err) {
-      console.log(err);
-  }
-  else {
-      console.log('Data Initialized!')
+    console.log(err);
+  } else {
+    console.log("Data Initialized!");
   }
 });
